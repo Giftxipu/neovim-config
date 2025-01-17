337d93b2e39dc1f939623fa84074abf98bce16d4
Gift Markus Xipu <gift.xipu@gmail.com>
2025-01-17 19:36:36 +0200

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,10 @@
 return {
     {
         "folke/tokyonight.nvim",
-        lazy = false, -- make sure we load this during startup
-        priority = 1000, -- load this before all other start plugins
+        lazy = false, 
+        priority = 1000, 
         config = function()
-            -- load the colorscheme here
             vim.cmd([[colorscheme tokyonight]])
-            
-            -- Optional: configure the theme variant
-            -- Available options: "storm" (default), "moon", "night", "day"
             vim.g.tokyonight_style = "storm"
         end,
     },
